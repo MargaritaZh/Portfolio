7984ce2fc9de2b4040b240760e04459ec0997829
MargaritaZh <margaritazhitkevich@gmail.com>
2024-03-02 00:35:38 +0300

    Skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
+import {Skills} from "./layout/sections/skills/Skills";
 
 
 
@@ -9,6 +10,7 @@ function App() {
         <div className="App">
             <Header/>
             <Main/>
+            <Skills/>
         </div>
     );
 }
