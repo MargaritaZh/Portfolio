b12cd94b5564cebb372457fd32bb61229028b6e8
MargaritaZh <margaritazhitkevich@gmail.com>
2024-03-02 15:30:41 +0300

    Works

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,33 +1,30 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const Menu = () => {
+// type MenuPropsType={
+//     menuItems:Array<string>
+// }
+
+export const Menu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledNav>
             <ul>
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">Skills</a>
-                </li>
-                <li>
-                    <a href="">Works</a>
-                </li>
-                <li>
-                    <a href="">Testimony</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
+                {props.menuItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a href="">{item}</a>
+                        </li>
+                    )
+                })}
             </ul>
         </StyledNav>
     );
 };
 
-const StyledNav=styled.nav`
+const StyledNav = styled.nav`
     ul {
         display: flex;
         gap: 30px;
+        justify-content: center;
     }
 `
