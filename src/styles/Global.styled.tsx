33012dd25f6fe74f211595bb76e410bcee9d3e4c
MargaritaZh <margaritazhitkevich@gmail.com>
2024-03-10 23:32:46 +0300

    theme

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -1,6 +1,7 @@
 import {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme";
 
-export const GlobalStyled=createGlobalStyle`
+export const GlobalStyled = createGlobalStyle`
     *,
     *::before,
     *::after {
@@ -17,20 +18,29 @@ export const GlobalStyled=createGlobalStyle`
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        color: ${theme.colors.font};
     }
 
     a {
-    text-decoration: none;
+        text-decoration: none;
     }
 
-    ul{
+    ul {
         list-style: none;
     }
-    
+
     button {
         background-color: unset;
         border: none;
     }
-    
-    
+
+    section:nth-of-type(odd) {
+        background-color: ${theme.colors.primaryBg};
+    }
+
+    section:nth-of-type(even) {
+        background-color: ${theme.colors.secondaryBg};
+    }
+
+
 `
